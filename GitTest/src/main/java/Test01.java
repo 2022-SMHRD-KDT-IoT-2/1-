2b29c86cb3edcfcd7a55167624e2ca6110dce9a4
smhrd <smhrd@59.0.129.102>
2022-02-22 17:12:32 +0900

    작업 공간 분리

--- GitTest/src/main/java/Test01.java
+++ GitTest/src/main/java/Test01.java
@@ -4,11 +4,7 @@ public class Test01 {
 	public static void main(String[] args) {
 		System.out.println("1번째 커밋");
 		System.out.println("2번째 커밋");
-		
-		
-		
-		
-		
+		System.out.println("3번째 커밋");
 	}
 
 }
